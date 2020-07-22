4d671c5f1da52a688af0393a5d2b37e6da50c1d5
Mr. Foxie <siddh.mistry396321@gmail.com>
2020-07-22 15:35:46 +0530

    Updated

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const bweb = require('bweb');
+const fs = require('bfile');
+const Validator = require('bval');
+const WSProxy = require('./wsproxy');
+
+const index = fs.readFileSync(`${__dirname}/index.html`);
+
+const proxy = new WSProxy({
+  ports: [8333, 18333, 48444, 18555]
+});
+
+const server = bweb.server({
+  host: '0.0.0.0',
+  port: 8080,
+  sockets: false,
+  ssl: false
+});
+
+server.use(server.router());
+
+server.on('error', (err) => {
+  console.error(err.stack);
+});
+
+server.get('/', (req, res) => {
+  res.send(200, index, 'html');
+});
+
+server.get('/favicon.ico', (req, res) => {
+  res.send(404);
+});
+
+server.get('/:file', (req, res) => {
+  const valid = Validator.fromRequest(req);
+  const file = valid.str('file');
+  res.send(200, getFile(file));
+});
+
+proxy.attach(server.http);
+
+server.open();
+
+function getFile(file){
+  return fs.readFileSync(`${__dirname}/${file}`)
+}
